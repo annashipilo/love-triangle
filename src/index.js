3abbf63f31eb941a80e90b80bcfea3b436380928
annashipilo <a.a.shipilo@gmail.com>
2018-02-25 13:32:39 +0300

    count triangles

--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+    var triangles = 0;
+  
+    for (var i = 0; i < preferences.length; i++) {
+    if (preferences[preferences[preferences[i] - 1] - 1] === i + 1) {
+        triangles++;
+      }
+    }
+
+  
+  return Math.floor(triangles / 3);
+  }
+
